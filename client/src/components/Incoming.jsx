28c649171fad2761a330b53b29b253c949d96523
JanicaJensen <122334098+JanicaJensen@users.noreply.github.com>
2023-08-01 22:55:15 -0600

    added homepage components, css, test homepage

---
+++ client/src/components/Incoming.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import '../styles/Incoming.css'
+
+export default function Incoming() {
+  return (
+    <>
+    <div className="Incoming">
+
+    <h3 className="Incoming">Incoming</h3>
+
+<table className="Incoming">
+  <thead>
+      <th>Student</th>
+      <th>Status</th>
+      <th>Reason</th>
+      <th>Teacher</th>
+  </thead>
+  <tbody>
+      <tr>
+          <td>Placeholder</td> 
+          <td>OUT/IN</td> 
+          <td>math help</td> 
+          <td>Snape</td> 
+      </tr>
+  </tbody>
+</table>
+
+
+
+
+    </div>
+     
+    
+    </>
+    
+  )
+}
---
+++ client/src/components/Outgoing.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import '../styles/Outgoing.css'
+
+export default function Outgoing() {
+  return (
+    <>
+    <div className="Outgoing">
+
+    <h3 className="Outgoing">Outgoing</h3>
+
+<table className="Outgoing">
+  <thead>
+      <th>Student</th>
+      <th>Status</th>
+      <th>Reason</th>
+      <th>Teacher</th>
+  </thead>
+  <tbody>
+      <tr>
+          <td>Placeholder</td> 
+          <td>OUT/IN</td> 
+          <td>math help</td> 
+          <td>Snape</td> 
+      </tr>
+  </tbody>
+</table>
+
+
+
+
+    </div>
+     
+    
+    </>
+    
+  )
+}
