77339de392c0b7a1e8dc18d99ea39ab539d954fc
Alexandra <awinterdrova@gmail.com>
2023-08-09 18:55:06 -0600

    added population for Student model and fixed the student model

--- server/model/Student.js
+++ server/model/Student.js
@@ -1,6 +1,6 @@
 const {Schema, Types, model} = require('mongoose');
 const { string } = require('yargs');
-const teacherSchema = require('./Teacher');
+//const teacherSchema = require('./Teacher');
 
 const studentSchema = new Schema(
     {
@@ -13,7 +13,7 @@ const studentSchema = new Schema(
         required: true,
     },
     //I am testing to see if the one to many relationship will hold the homeroom teacher like this.
-    homeroomTeacher: [teacherSchema],
+    //homeroomTeacher: [teacherSchema],
     onTheWay: {
         type: Boolean,
         default : false,
@@ -30,5 +30,28 @@ const studentSchema = new Schema(
 );
 
 const Student = model('Student', studentSchema);
+
+Studentlist = [
+    {studentName:"Kyler", onTheWay:false, arrived:false, requested:true},
+    {studentName:"Brenda", onTheWay:false, arrived:false, requested:true},
+    {studentName:"Sam", onTheWay:false, arrived:false, requested:true},
+    {studentName:"Jenna", onTheWay:false, arrived:false, requested:true},
+    {studentName:"Laura", onTheWay:false, arrived:false, requested:true},
+    {studentName:"Jeremy", onTheWay:false, arrived:false, requested:true},
+];
+
+Student.find({}).exec().then(async collection =>{
+    if(collection.length === 0){
+        const results = await Student.insertMany(
+            Studentlist
+        );
+        return console.log('Students Successfully inserted', results);
+    }
+    return console.log('We have enough test data');
+}).catch(err => console.log(err));
+
+
+
+const handleError = (err) => console.error(err);
 
 module.exports = Student;
